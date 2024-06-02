473257ad460f94d5ea878080c823af5bf6d89cb8
Jaspr3m <jasprem.singh@hva.nl>
2024-06-02 19:47:32 +0200

    Removed logs, changable name

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Page loaded!")
 
-// KNOPPEN ----------
-// Pizza knoppen
+// Interactie ----------
+// Pizza interactie methodes
 const thinCrustKnop = document.querySelector("button#thin-crust-button")
 const thickCrustKnop = document.querySelector("button#thick-crust-button")
 const marinaraKnop = document.querySelector("button#marinara-button")
@@ -9,9 +9,12 @@ const chocolateKnop = document.querySelector("button#chocolate-button")
 const pepperoniKnop = document.querySelector("button#pepperoni-button")
 const pineappleKnop = document.querySelector("button#pineapple-button")
 
-// Progress bar + knop [TEST]
+// Healthbar interactie methodes
 const addHealthKnop = document.querySelector("button#addHealth")
 const removeHealthKnop = document.querySelector("button#removeHealth")
+
+// Naamgeving interactie methodes
+const bevestigNaamKnop = document.querySelector("button#bevestigNaam")
 
 // IMG ----------------
 // Img's die van de overview moeten veranderen
@@ -54,10 +57,20 @@ function selecteerPineapple() {
     audio.play();
 }
 
+// NAAMGEVING
+// Via inputveld, verander naam van de pizza en weergeef in H2
+let naam = "Jasprem"
+let h2 = document.querySelector("h2")
+
+function veranderNaam() {
+    naam = document.getElementById("naamPizza").value
+    console.log(naam)
+}
+
 // PIZZA HEALTHBAR
 // Variabelen aanmaken
 let progressBar = document.querySelector("#progressBar")
-let lifeIndicator = document.querySelector("h2#life-indicator")
+let lifeIndicator = document.querySelector("h2")
 let health = 100
 
 function healthDrain() {
@@ -65,7 +78,7 @@ function healthDrain() {
     if (health < 100 && health > 1) {
         health = health - 1
         progressBar.value = health
-        lifeIndicator.innerHTML = "HIJ LEEFT!!!"
+        lifeIndicator.innerHTML = naam.toUpperCase() + " LEEFT!!!"
 
         crustImg.classList.add("enable")
         sauceImg.classList.add("enable")
@@ -85,14 +98,10 @@ function healthDrain() {
     } else if (health <= 1) {
         health = 1
         progressBar.value = health
-        lifeIndicator.innerHTML = "HIJ IS DOOD!! REVIVE HEM!!!"
+        lifeIndicator.innerHTML = naam.toUpperCase() + " IS DOOD!! REVIVE " + naam.toUpperCase() + "!!!" 
         crustImg.classList.add("disable")
         sauceImg.classList.add("disable")
         toppingImg.classList.add("disable")
-
-        crustImg.classList.remove("enable")
-        sauceImg.classList.remove("enable")
-        toppingImg.classList.remove("enable")
     } 
 }
 // voer function healthDrain() elke 300 ms uit
@@ -126,7 +135,7 @@ pineappleKnop.addEventListener("click", selecteerPineapple)
 // Progress bar event listener
 addHealthKnop.addEventListener("click", addHealth)
 removeHealthKnop.addEventListener("click", removeHealth)
-
+bevestigNaamKnop.addEventListener("click", veranderNaam)
 // Progress bar
 
 
