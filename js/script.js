002e6b1621e292b2738531e6cf4c124e7219b79a
Jaspr3m <jasprem.singh@hva.nl>
2024-06-03 16:26:54 +0200

    Added more buttons.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-console.log("Page loaded!")
-
 // Interactie ----------
 // Pizza interactie methodes
 const thinCrustKnop = document.querySelector("button#thin-crust-button")
 const thickCrustKnop = document.querySelector("button#thick-crust-button")
 const marinaraKnop = document.querySelector("button#marinara-button")
 const chocolateKnop = document.querySelector("button#chocolate-button")
+const buffaloKnop = document.querySelector("button#buffalo-button")
+const bbqKnop = document.querySelector("button#bbq-button")
+const oliveKnop = document.querySelector("button#olive-button")
+const anchovisKnop = document.querySelector("button#anchovis-button")
 const pepperoniKnop = document.querySelector("button#pepperoni-button")
 const pineappleKnop = document.querySelector("button#pineapple-button")
 
@@ -40,6 +42,22 @@ function selecteerChocolate() {
     sauceImg.src = "img/chocolate.png";
 }
 
+function selecteerBuffalo() {
+    sauceImg.src = "img/buffalo.png";
+}
+
+function selecteerBBQ() {
+    sauceImg.src = "img/bbq.png";
+}
+
+function selecteerOlive() {
+    toppingImg.src = "img/olive.png";
+}
+
+function selecteerAnchovis() {
+    toppingImg.src = "img/anchovis.png";
+}
+
 function selecteerPepperoni() {
     toppingImg.src = "img/pepperoni.png";
 }
@@ -58,7 +76,6 @@ let h2 = document.querySelector("h2")
 
 function veranderNaam() {
     naam = document.getElementById("naamPizza").value
-    console.log(naam)
 }
 
 // PIZZA HEALTHBAR
@@ -68,16 +85,13 @@ let progressBar = document.querySelector("#progressBar")
 let lifeIndicator = document.querySelector("h2")
 let healthAdded = document.querySelector("h3")
 
+// Functie die ervoor zorgt dat er health continu afgaat per 300ms
 function healthDrain() {
-    // health gaat met -1 elke 100ms ervan af
+    // health gaat met -1 elke 300ms ervan af
     if (health < 100 && health > 1) {
         health = health - 1
         progressBar.value = health
         lifeIndicator.innerHTML = naam.toUpperCase() + " LEEFT!!!"
-
-        crustImg.classList.add("enable")
-        sauceImg.classList.add("enable")
-        toppingImg.classList.add("enable")
 
         crustImg.classList.remove("disable")
         sauceImg.classList.remove("disable")
@@ -86,14 +100,11 @@ function healthDrain() {
     } else if (health >= 100) {
         health--
         progressBar.value = health
-        crustImg.classList.add("enable")
-        sauceImg.classList.add("enable")
-        toppingImg.classList.add("enable")
         // wanneer health meer dan of gelijk aan 1 is, health blijft gelijk aan 1
     } else if (health <= 1) {
         health = 1
         progressBar.value = health
-        lifeIndicator.innerHTML = naam.toUpperCase() + " IS DOOD GEGAAN!! HOE KON JE!?!?! "
+        lifeIndicator.innerHTML = naam.toUpperCase() + " IS DOOD GEGAAN!! HOE KON JE!?!?!"
         crustImg.classList.add("disable")
         sauceImg.classList.add("disable")
         toppingImg.classList.add("disable")
@@ -128,6 +139,10 @@ thinCrustKnop.addEventListener("click", selecteerThinCrust)
 thickCrustKnop.addEventListener("click", selecteerThickCrust)
 marinaraKnop.addEventListener("click", selecteerMarinara)
 chocolateKnop.addEventListener("click", selecteerChocolate)
+buffaloKnop.addEventListener("click", selecteerBuffalo)
+bbqKnop.addEventListener("click", selecteerBBQ)
+oliveKnop.addEventListener("click", selecteerOlive)
+anchovisKnop.addEventListener("click", selecteerAnchovis)
 pepperoniKnop.addEventListener("click", selecteerPepperoni)
 pineappleKnop.addEventListener("click", selecteerPineapple)
 
@@ -135,6 +150,5 @@ pineappleKnop.addEventListener("click", selecteerPineapple)
 addHealthKnop.addEventListener("click", addHealth)
 removeHealthKnop.addEventListener("click", removeHealth)
 bevestigNaamKnop.addEventListener("click", veranderNaam)
-// Progress bar
 
 
